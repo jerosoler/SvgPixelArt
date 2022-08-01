af7cd853510a809877c7f5f89c3b8f3ac3164987
Jero Soler <jero.soler@gmail.com>
2022-08-01 13:03:56 +0200

    Fix Gh-page3

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: "static",
   router: {
-    base: 'SvgPixelArt'
+    base: '/SvgPixelArt/'
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
