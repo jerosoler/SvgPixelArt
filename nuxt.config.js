66e3fb11be7677aa32aeb26c2ba32b2a9ed73383
Jero Soler <jero.soler@gmail.com>
2022-08-01 12:48:08 +0200

    Fix to gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: "static",
+  publicRuntimeConfig: {
+    baseURL: process.env.NUXT_BASE_URL || 'https://jerosoler.github.io/SvgPixelArt/'
+  },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
